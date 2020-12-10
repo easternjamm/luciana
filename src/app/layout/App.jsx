718feb9e3e0943f1aa71aa06a8bb73f0c88db029
EasternJamm <apetremarianioan@gmail.com>
2020-12-10 20:27:00 +0200

    Event Form and create event

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,13 +1,19 @@
-import {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>LucianaSasu</h1>
-      </div>
+      <Fragment>
+        <NavBar/>
+        <Container className="main">
+          <EventDashboard/>
+         </Container>
+      </Fragment>
     );
-  } 
+  }
 }
 
 export default App;
